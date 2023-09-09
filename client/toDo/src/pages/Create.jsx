26c539915f91ca721bc941dc945373564a3fd3f2
Justin M <manningjustin08@yahoo.com>
2023-09-08 21:59:52 -0400

    Working on MongoDB connections

--- client/toDo/src/pages/Create.jsx
+++ client/toDo/src/pages/Create.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function Create() {
+    const [task, setTask] = useState()
+    const handleAdd = () => {
+       axios.post("http://localhost:3000/add", {task: task})
+       .then(result => console.log(result))
+       .catch(err => console.log(err))
+    }
+
   return (
     <div>
-        <input type='text' name='' id='task'/>
-        <button type='button' className='btn'>Add Task</button>
+        <input type='text' placeholder='what do you wanna do?...' onChange={(e) => setTask(e.target.value)} id='task'/>
+        <button type='button' onClick={handleAdd} className='btn'>Add Task</button>
     </div>
   )
 }
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const TodoModel = require('./backend/Models/Todo')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json)
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!")
