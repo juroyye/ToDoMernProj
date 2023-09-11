5f3559d624d6ec0865e4498e0c955c8af3e87911
Justin M <manningjustin08@yahoo.com>
2023-09-11 03:22:20 -0400

    Constructed frontend authentication form and successfully tested it for functionality

--- client/toDo/src/App.js
+++ client/toDo/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import React from 'react';
-// import LoginForm from './pages/Login';
-import ToDoForm from './pages/Todos';
+import LoginForm from './pages/Login';
+// import ToDoForm from './pages/Todos';
 
 function App() {
 
   return (
 <>   
-      <header id='title'>My Tasks</header>
+      {/* <header id='title'>My Tasks</header>
         <div id='tdform'> 
           <ToDoForm/>
-          </div>    
+          </div>     */}
       
-      {/* <div className='form'>
+      <div className='form'>
          <LoginForm />
-         </div> */}
+         </div>
 
              </>            
   );
--- client/toDo/src/pages/Login.jsx
+++ client/toDo/src/pages/Login.jsx
@@ -1,30 +1,40 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 function LoginForm() {
-    // State to store the values of email and password inputs
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    // Function to handle form submission
-    const handleSubmit = (e) => {
-      e.preventDefault(); // Prevent the default form submission behavior
-      // You can add your logic here, such as sending the data to a server
-      console.log('Email:', email);
-      console.log('Password:', password);
-    };
-  
-    return(
-        <div>
-            <h1 id="header">
-                Let's Do More
-            </h1>
+  // State to store the values of email and password inputs
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // State to store error messages
 
-            <p>Login</p>
-            
-            <form onSubmit={handleSubmit}>
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    // Hardcoded email and password
+    const hardcodedEmail = "manningjustin08@yahoo.com";
+    const hardcodedPassword = "password";
+
+    // Check if the entered email and password match the hardcoded values
+    if (email === hardcodedEmail && password === hardcodedPassword) {
+      // Successful login
+      setError(""); // Clear any previous error message
+      console.log("Login successful!");
+      // You can add redirection or further logic here for authenticated users
+    } else {
+      // Failed login
+      setError("Invalid email or password"); // Set an error message
+    }
+  };
+
+  return (
+    <div>
+      <h1 id="header">Let's Do More</h1>
+
+      <p>Login</p>
+
+      <form onSubmit={handleSubmit}>
         <div>
-          <label email="email">Email:</label>
+          <label htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
@@ -35,7 +45,7 @@ function LoginForm() {
           />
         </div>
         <div>
-          <label password="password">Password:</label>
+          <label htmlFor="password">Password:</label>
           <input
             type="password"
             id="password"
@@ -49,8 +59,10 @@ function LoginForm() {
           <button type="submit">Submit</button>
         </div>
       </form>
-        </div>
-    )
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
 }
 
 export default LoginForm;
